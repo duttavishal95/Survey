a7946c093a58e57ffc496f6e91fce44999412ea6
Vishal <>
2019-03-01 14:25:19 -0500

    Added String entry in survey

--- app/src/main/java/com/survey/SurveyActivity.java
+++ app/src/main/java/com/survey/SurveyActivity.java
@@ -3,8 +3,11 @@ package com.survey;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -13,14 +16,26 @@ public class SurveyActivity extends AppCompatActivity implements View.OnClickLis
     private RadioGroup firstGroup;
     private Button submit;
 
+    private RadioButton excellent;
+    private RadioButton veryGood;
+    private RadioButton good;
+    private RadioButton bad;
+
+    private EditText feedback;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
 
         firstGroup = findViewById(R.id.firstSurvey);
-        submit = findViewById(R.id.submit);
+        excellent = findViewById(R.id.excellent);
+        veryGood = findViewById(R.id.very_good);
+        good = findViewById(R.id.good);
+        bad = findViewById(R.id.bad);
+        feedback = findViewById(R.id.answer);
 
+        submit = findViewById(R.id.submit);
         submit.setOnClickListener(this);
     }
 
@@ -32,6 +47,9 @@ public class SurveyActivity extends AppCompatActivity implements View.OnClickLis
                 if (firstGroup.getCheckedRadioButtonId() == -1) {
                     showMessageToast("Select any option");
                     return;
+                } else if (TextUtils.isEmpty(feedback.getText().toString())) {
+                    showMessageToast("Enter feedback");
+                    return;
                 }
                 showMessageToast("Survey Submitted");
                 break;
@@ -41,5 +59,4 @@ public class SurveyActivity extends AppCompatActivity implements View.OnClickLis
     private void showMessageToast(String toastMessage) {
         Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
     }
-
 }
